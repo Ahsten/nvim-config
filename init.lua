872ae4f6e5c7d06c3eb2e68c68196b81b8c19588
Ahsten <ahanson188@gmail.com>
2025-09-15 18:40:40 -0500

    Changed to gruvbox theme

--- init.lua
+++ init.lua
@@ -899,14 +899,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'miikanissi/modus-themes.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd [[colorscheme modus_vivendi]] -- modus_operandi, modus_vivendi
+      vim.cmd [[colorscheme gruvbox]]
     end,
   },
 

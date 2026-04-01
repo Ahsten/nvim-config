098d20e33af8501b5e018e31b00aec5189c8c13f
Luxe17 <hanson1717@live.com>
2026-03-31 19:02:56 -0500

    Added lsp settings

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 -- ========================================================================== --
 -- ==                           EDITOR SETTINGS                            == --
 -- ========================================================================== --
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.ignorecase = true
@@ -20,32 +19,31 @@ vim.o.winborder = 'rounded'
 vim.g.mapleader = vim.keycode('<Space>')
 
 -- Basic clipboard interaction
-vim.keymap.set({'n', 'x'}, 'gy', '"+y', {desc = 'Copy to clipboard'})
-vim.keymap.set({'n', 'x'}, 'gp', '"+p', {desc = 'Paste clipboard content'})
+vim.keymap.set({ 'n', 'x' }, 'gy', '"+y', { desc = 'Copy to clipboard' })
+vim.keymap.set({ 'n', 'x' }, 'gp', '"+p', { desc = 'Paste clipboard content' })
 
 -- ========================================================================== --
 -- ==                               PLUGINS                                == --
 -- ========================================================================== --
 
 -- NOTE: To install a plugin you just need to add the URL to the repository.
--- But as soon as you need to add more information, like the git branch or 
+-- But as soon as you need to add more information, like the git branch or
 -- commit, use the "plugin spec" form. See :help vim.pack
 
 vim.pack.add({
-  'https://github.com/folke/tokyonight.nvim',
-  'https://github.com/folke/which-key.nvim',
-  'https://github.com/folke/snacks.nvim',
-  'https://github.com/VonHeikemen/ts-enable.nvim',
-  'https://github.com/neovim/nvim-lspconfig',
-  {src = 'https://github.com/nvim-mini/mini.nvim', version = 'main'},
-  {src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main'},
+	'https://github.com/ellisonleao/gruvbox.nvim',
+	'https://github.com/folke/which-key.nvim',
+	'https://github.com/folke/snacks.nvim',
+	'https://github.com/neovim/nvim-lspconfig',
+	{ src = 'https://github.com/nvim-mini/mini.nvim',             version = 'main' },
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
 })
 
 -- ========================================================================== --
 -- ==                         PLUGIN CONFIGURATION                         == --
 -- ========================================================================== --
 
-vim.cmd.colorscheme('tokyonight-moon')
+vim.cmd.colorscheme('gruvbox')
 
 -- See :help MiniSurround.config
 require('mini.surround').setup({})
@@ -54,31 +52,30 @@ require('mini.pairs').setup({})
 
 -- See :help MiniNotify.config
 require('mini.notify').setup({
-  lsp_progress = {enable = false},
+	lsp_progress = { enable = false },
 })
 
 -- See :help MiniBufremove.config
 require('mini.bufremove').setup({})
 
 -- Close buffer and preserve window layout
-vim.keymap.set('n', '<leader>bc', '<cmd>lua pcall(MiniBufremove.delete)<cr>', {desc = 'Close buffer'})
+vim.keymap.set('n', '<leader>bc', '<cmd>lua pcall(MiniBufremove.delete)<cr>', { desc = 'Close buffer' })
 
-require('snacks').setup({
+local Snacks = require('snacks')
+
+Snacks.setup({
 	explorer = { enabled = true, replace_netrw = true },
 	picker = { enabled = true },
-	keys = {
-
-	},
 })
 
-
-vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end, {desc = "Buffers" })
-vim.keymap.set("n", "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, {desc = "Find Config File"})
-vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, {desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", function() Snacks.picker.git_files() end, {desc = "Find Git Files"})
-vim.keymap.set("n", "<leader>fp", function() Snacks.picker.projects() end, {desc = "Projects" })
-vim.keymap.set("n", "<leader>fr", function() Snacks.picker.recent() end, {desc = "Recent" })
-vim.keymap.set("n", "<leader>e", function() Snacks.picker.explorer() end, {desc = "Explorer"})
+vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end,
+	{ desc = "Find Config File" })
+vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", function() Snacks.picker.git_files() end, { desc = "Find Git Files" })
+vim.keymap.set("n", "<leader>fp", function() Snacks.picker.projects() end, { desc = "Projects" })
+vim.keymap.set("n", "<leader>fr", function() Snacks.picker.recent() end, { desc = "Recent" })
+vim.keymap.set("n", "<leader>e", function() Snacks.picker.explorer() end, { desc = "Explorer" })
 
 
 -- See :help MiniStatusline.config
@@ -86,66 +83,70 @@ require('mini.statusline').setup({})
 
 -- See :help MiniCompletion.config
 require('mini.completion').setup({
-  lsp_completion = {
-    source_func = 'omnifunc',
-    auto_setup = false,
-  },
+	lsp_completion = {
+		source_func = 'omnifunc',
+		auto_setup = false,
+	},
 })
 
 -- See :help which-key.nvim-which-key-setup
 require('which-key').setup({
-  preset = 'helix',
-  icons = {
-    mappings = false,
-    keys = {
-      Space = 'Space',
-      Esc = 'Esc',
-      BS = 'Backspace',
-      C = 'Ctrl-',
-    },
-  },
+	preset = 'helix',
+	icons = {
+		mappings = false,
+		keys = {
+			Space = 'Space',
+			Esc = 'Esc',
+			BS = 'Backspace',
+			C = 'Ctrl-',
+		},
+	},
 })
 
 require('which-key').add({
-  {'<leader>f', group = 'Fuzzy Find'},
-  {'<leader>b', group = 'Buffer'},
+	{ '<leader>f', group = 'Fuzzy Find' },
+	{ '<leader>b', group = 'Buffer' },
 })
 
 -- Treesitter setup
 -- NOTE: the list of supported parsers is in the documentation:
 -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
-local ts_parsers = {'lua', 'vim', 'vimdoc', 'c', 'query'}
-
--- See :help ts-enable-config
-vim.g.ts_enable = {
-  parsers = ts_parsers,
-  auto_install = true,
-  highlights = true,
-}
+local ts_parsers = { 'lua', 'vim', 'vimdoc', 'c', 'query', 'zig' }
 
 -- Try to update all parsers after a plugin update
 vim.api.nvim_create_autocmd('PackChanged', {
-  pattern = 'nvim-treesitter',
-  desc = 'Update treesitter parsers',
-  command = 'TSUpdate'
+	pattern = 'nvim-treesitter',
+	desc = 'Update treesitter parsers',
+	command = 'TSUpdate'
 })
 
 -- LSP setup
 vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+	desc = 'LSP actions',
+	callback = function(event)
+		local opts = { buffer = event.buf }
+		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+		vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+		vim.keymap.set({ 'n', 'x' }, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
 
-    local id = vim.tbl_get(event, 'data', 'client_id')
-    local client = id and vim.lsp.get_client_by_id(id)
+		local id = vim.tbl_get(event, 'data', 'client_id')
+		local client = id and vim.lsp.get_client_by_id(id)
 
-    if client and client:supports_method('textDocument/completion') then
-      vim.bo[event.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
-    end
-  end,
+		if client and client:supports_method('textDocument/completion') then
+			vim.bo[event.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
+		end
+	end,
 })
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+})
+
+
+vim.lsp.enable({
+	"lua_ls",
+	"zls"
+})

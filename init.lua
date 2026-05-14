ce9df7494276092e7a6dc3ed37ac43eeb2605bff
Luxe17 <hanson1717@live.com>
2026-05-14 18:31:43 -0500

    Revert "Refactor config"
    This reverts commit bad99307ce21488e20bf9b7a534afb9bf127f2cc.

--- init.lua
+++ init.lua
@@ -1,160 +1,151 @@
----@diagnostic disable: missing-fields
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.undofile = true
-vim.opt.signcolumn = "yes"
-vim.opt.inccommand = "split"
-vim.opt.cursorline = true
-vim.opt.hlsearch = true
-vim.opt.wrap = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.textwidth = 80
+-- ========================================================================== --
+-- ==                           EDITOR SETTINGS                            == --
+-- ========================================================================== --
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = false
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.showmode = false
+vim.o.termguicolors = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.signcolumn = 'yes'
+vim.o.winborder = 'rounded'
+
+-- Space as leader key
+vim.g.mapleader = vim.keycode('<Space>')
+
+-- Basic clipboard interaction
+vim.keymap.set({ 'n', 'x' }, 'gy', '"+y', { desc = 'Copy to clipboard' })
+vim.keymap.set({ 'n', 'x' }, 'gp', '"+p', { desc = 'Paste clipboard content' })
+
+-- ========================================================================== --
+-- ==                               COMMANDS                               == --
+-- ========================================================================== --
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight on yank',
+  callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
+  end,
+})
+
+-- ========================================================================== --
+-- ==                               PLUGINS                                == --
+-- ========================================================================== --
+
+-- NOTE: To install a plugin you just need to add the URL to the repository.
+-- But as soon as you need to add more information, like the git branch or
+-- commit, use the "plugin spec" form. See :help vim.pack
+
+vim.pack.add({
+	'https://github.com/folke/tokyonight.nvim',
+	'https://github.com/folke/which-key.nvim',
+	'https://github.com/folke/snacks.nvim',
+	'https://github.com/neovim/nvim-lspconfig',
+	{ src = 'https://github.com/nvim-mini/mini.nvim',             version = 'main' },
+})
+
+-- ========================================================================== --
+-- ==                         PLUGIN CONFIGURATION                         == --
+-- ========================================================================== --
+
+vim.cmd.colorscheme('tokyonight-night')
+
+-- See :help MiniSurround.config
+require('mini.surround').setup({})
+
+require('mini.pairs').setup({})
+
+-- See :help MiniNotify.config
+require('mini.notify').setup({
+	lsp_progress = { enable = false },
+})
+
+-- See :help MiniBufremove.config
+require('mini.bufremove').setup({})
+
+-- Close buffer and preserve window layout
+vim.keymap.set('n', '<leader>bc', '<cmd>lua pcall(MiniBufremove.delete)<cr>', { desc = 'Close buffer' })
+
+local Snacks = require('snacks')
+
+Snacks.setup({
+	explorer = { enabled = true, replace_netrw = true },
+	picker = { enabled = true },
+})
+
+vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end,
+	{ desc = "Find Config File" })
+vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", function() Snacks.picker.git_files() end, { desc = "Find Git Files" })
+vim.keymap.set("n", "<leader>fp", function() Snacks.picker.projects() end, { desc = "Projects" })
+vim.keymap.set("n", "<leader>fr", function() Snacks.picker.recent() end, { desc = "Recent" })
+vim.keymap.set("n", "<leader>e", function() Snacks.picker.explorer() end, { desc = "Explorer" })
+
+
+-- See :help MiniStatusline.config
+require('mini.statusline').setup({})
+
+-- See :help MiniCompletion.config
+require('mini.completion').setup({
+	lsp_completion = {
+		source_func = 'omnifunc',
+		auto_setup = false,
+	},
+})
+
+-- See :help which-key.nvim-which-key-setup
+require('which-key').setup({
+	icons = {
+		mappings = false,
+		keys = {
+			Space = 'Space',
+			Esc = 'Esc',
+			BS = 'Backspace',
+			C = 'Ctrl-',
+		},
+	},
+})
+
+require('which-key').add({
+	{ '<leader>f', group = 'Fuzzy Find' },
+	{ '<leader>b', group = 'Buffer' },
+})
+
+
+-- LSP setup
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'LSP actions',
+	callback = function(event)
+		local opts = { buffer = event.buf }
+		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+		vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+		vim.keymap.set({ 'n', 'x' }, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+
+		local id = vim.tbl_get(event, 'data', 'client_id')
+		local client = id and vim.lsp.get_client_by_id(id)
+
+		if client and client:supports_method('textDocument/completion') then
+			vim.bo[event.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
+		end
+	end,
+})
 
 vim.diagnostic.config({
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = " ",
-      [vim.diagnostic.severity.WARN] = " ",
-      [vim.diagnostic.severity.INFO] = " ",
-      [vim.diagnostic.severity.HINT] = " ",
-    },
-  },
-  virtual_text = true, -- show inline diagnostics
+  virtual_text = true,
+  signs = true,
+  underline = true,
 })
 
--- clear search highlights with <Esc>
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- INFO: colorscheme
-vim.pack.add({ 'https://github.com/ellisonleao/gruvbox.nvim' })
-vim.cmd.colorscheme("gruvbox")
-
--- INFO: formatting and syntax highlighting
-vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" }, { confirm = false })
-
--- equivalent to :TSUpdate
-require("nvim-treesitter.install").update("all")
-
--- INFO: completion engine
-vim.pack.add({ "https://github.com/saghen/blink.cmp" }, { confirm = false })
-
-require("blink.cmp").setup({
-  completion = {
-    documentation = {
-      auto_show = true,
-    },
-  },
-
-  -- default blink keymaps
-  keymap = {
-    ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-    ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-
-    ['<C-y>'] = { 'select_and_accept', 'fallback' },
-    ['<C-e>'] = { 'cancel', 'fallback' },
-    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-
-    ['<Tab>'] = { 'snippet_forward', 'fallback' },
-    ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-
-    ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-    ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-
-    ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
-  },
-
-  fuzzy = {
-    implementation = "lua",
-  },
+vim.lsp.enable({
+	"lua_ls",
+	"ts_ls",
+	"zls",
 })
-
--- INFO: lsp server installation and configuration
-
--- lsp servers we want to use and their configuration
--- see `:h lspconfig-all` for available servers and their settings
-local lsp_servers = {
-  lua_ls = {
-    -- https://luals.github.io/wiki/settings/ | `:h nvim_get_runtime_file`
-    Lua = { workspace = { library = vim.api.nvim_get_runtime_file("lua", true) }, },
-  },
-  clangd = {},
-  -- rust_analyzer = {},
-  -- gopls = {},
-  ts_ls = {},
-}
-
-vim.pack.add({
-  "https://github.com/neovim/nvim-lspconfig", -- default configs for lsps
-
-  -- NOTE: if you'd rather install the lsps through your OS package manager you
-  -- can delete the next three mason-related lines and their setup calls below.
-  -- see `:h lsp-quickstart` for more details.
-  "https://github.com/mason-org/mason.nvim",                     -- package manager
-  "https://github.com/mason-org/mason-lspconfig.nvim",           -- lspconfig bridge
-  "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" -- auto installer
-}, { confirm = false })
-
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-tool-installer").setup({
-  ensure_installed = vim.tbl_keys(lsp_servers),
-})
-
--- configure each lsp server on the table
--- to check what clients are attached to the current buffer, use
--- `:checkhealth vim.lsp`. to view default lsp keybindings, use `:h lsp-defaults`.
-for server, config in pairs(lsp_servers) do
-  vim.lsp.config(server, {
-    settings = config,
-
-    -- only create the keymaps if the server attaches successfully
-    on_attach = function(_, bufnr)
-      vim.keymap.set("n", "grd", vim.lsp.buf.definition,
-        { buffer = bufnr, desc = "vim.lsp.buf.definition()", })
-
-      vim.keymap.set("n", "grf", vim.lsp.buf.format,
-        { buffer = bufnr, desc = "vim.lsp.buf.format()", })
-    end,
-  })
-end
-
--- INFO: fuzzy finder
-vim.pack.add({
-  "https://github.com/nvim-lua/plenary.nvim",        -- library dependency
-  "https://github.com/nvim-tree/nvim-web-devicons",  -- icons (nerd font)
-  "https://github.com/nvim-telescope/telescope.nvim" -- the fuzzy finder
-}, { confirm = false })
-
-require("telescope").setup({})
-
-local pickers = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>sp", pickers.builtin, { desc = "[S]earch Builtin [P]ickers", })
-vim.keymap.set("n", "<leader>sb", pickers.buffers, { desc = "[S]earch [B]uffers", })
-vim.keymap.set("n", "<leader>sf", pickers.find_files, { desc = "[S]earch [F]iles", })
-vim.keymap.set("n", "<leader>sw", pickers.grep_string, { desc = "[S]earch Current [W]ord", })
-vim.keymap.set("n", "<leader>sg", pickers.live_grep, { desc = "[S]earch by [G]rep", })
-vim.keymap.set("n", "<leader>sr", pickers.resume, { desc = "[S]earch [R]esume", })
-
-vim.keymap.set("n", "<leader>sh", pickers.help_tags, { desc = "[S]earch [H]elp", })
-vim.keymap.set("n", "<leader>sm", pickers.man_pages, { desc = "[S]earch [M]anuals", })
-
--- INFO: keybinding helper
-vim.pack.add({ "https://github.com/folke/which-key.nvim" }, { confirm = false })
-
-require("which-key").setup({
-  preset = "helix",
-  spec = {
-    { "<leader>s", group = "[S]earch", icon = { icon = "", color = "green", }, },
-  }
-})
-
--- uncomment to enable automatic plugin updates
--- vim.pack.update()

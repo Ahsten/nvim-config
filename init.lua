bc18b6ce08db5d745443c9512f81fa8a1b4f5aeb
Luxe17 <hanson1717@live.com>
2026-05-15 08:46:00 -0500

    Update lsp config

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 vim.o.signcolumn = 'yes'
 vim.o.winborder = 'rounded'
+vim.o.cursorline = true
 
 -- Space as leader key
 vim.g.mapleader = vim.keycode('<Space>')
@@ -27,10 +28,17 @@ vim.keymap.set({ 'n', 'x' }, 'gp', '"+p', { desc = 'Paste clipboard content' })
 -- ========================================================================== --
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight on yank',
-  callback = function()
-    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
-  end,
+	desc = 'Highlight on yank',
+	callback = function()
+		vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType",{
+	pattern = "snacks_picker_input",
+	desc = "Disable mini.completion for snacks picker",
+	group = vim.api.nvim_create_augroup("user_mini",{}),
+	command = "lua vim.b.minicompletion_disable=true",
 })
 
 -- ========================================================================== --
@@ -43,6 +51,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.pack.add({
 	'https://github.com/folke/tokyonight.nvim',
+	'https://github.com/ellisonleao/gruvbox.nvim',
 	'https://github.com/folke/which-key.nvim',
 	'https://github.com/folke/snacks.nvim',
 	'https://github.com/neovim/nvim-lspconfig',
@@ -65,12 +74,6 @@ require('mini.notify').setup({
 	lsp_progress = { enable = false },
 })
 
--- See :help MiniBufremove.config
-require('mini.bufremove').setup({})
-
--- Close buffer and preserve window layout
-vim.keymap.set('n', '<leader>bc', '<cmd>lua pcall(MiniBufremove.delete)<cr>', { desc = 'Close buffer' })
-
 local Snacks = require('snacks')
 
 Snacks.setup({
@@ -79,12 +82,11 @@ Snacks.setup({
 })
 
 vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end,
-	{ desc = "Find Config File" })
+vim.keymap.set("n", "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, { desc = "Find Config File" })
 vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", function() Snacks.picker.git_files() end, { desc = "Find Git Files" })
+vim.keymap.set("n", "<leader>/", function() Snacks.picker.grep() end, { desc = "Grep" })
 vim.keymap.set("n", "<leader>fp", function() Snacks.picker.projects() end, { desc = "Projects" })
-vim.keymap.set("n", "<leader>fr", function() Snacks.picker.recent() end, { desc = "Recent" })
+vim.keymap.set("n", "<leader>r", function() Snacks.picker.resume() end, { desc = "Resume" })
 vim.keymap.set("n", "<leader>e", function() Snacks.picker.explorer() end, { desc = "Explorer" })
 
 
@@ -95,12 +97,12 @@ require('mini.statusline').setup({})
 require('mini.completion').setup({
 	lsp_completion = {
 		source_func = 'omnifunc',
-		auto_setup = false,
 	},
 })
 
 -- See :help which-key.nvim-which-key-setup
 require('which-key').setup({
+	preset = "helix",
 	icons = {
 		mappings = false,
 		keys = {
@@ -118,34 +120,60 @@ require('which-key').add({
 })
 
 
--- LSP setup
-vim.api.nvim_create_autocmd('LspAttach', {
-	desc = 'LSP actions',
-	callback = function(event)
-		local opts = { buffer = event.buf }
-		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-		vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-		vim.keymap.set({ 'n', 'x' }, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
 
-		local id = vim.tbl_get(event, 'data', 'client_id')
-		local client = id and vim.lsp.get_client_by_id(id)
+local lsp_servers = {
+	lua_ls = {
+		-- https://luals.github.io/wiki/settings/ | `:h nvim_get_runtime_file`
+		Lua = { workspace = { library = vim.api.nvim_get_runtime_file("lua", true) }, },
+	},
+	-- clangd = {},
+	-- rust_analyzer = {},
+	-- gopls = {},
+	-- zls = {},
+	ts_ls = {},
+}
 
-		if client and client:supports_method('textDocument/completion') then
-			vim.bo[event.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
-		end
-	end,
+vim.pack.add({
+	"https://github.com/neovim/nvim-lspconfig", -- default configs for lsps
+
+	"https://github.com/mason-org/mason.nvim",                     -- package manager
+	"https://github.com/mason-org/mason-lspconfig.nvim",           -- lspconfig bridge
+	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" -- auto installer
+}, { confirm = false })
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-tool-installer").setup({
+	ensure_installed = vim.tbl_keys(lsp_servers),
 })
 
+-- configure each lsp server on the table
+-- to check what clients are attached to the current buffer, use
+-- `:checkhealth vim.lsp`. to view default lsp keybindings, use `:h lsp-defaults`.
+for server, config in pairs(lsp_servers) do
+	vim.lsp.config(server, {
+		settings = config,
+
+		-- only create the keymaps if the server attaches successfully
+		on_attach = function(_, bufnr)
+			vim.keymap.set("n", "grd", vim.lsp.buf.definition,
+			{ buffer = bufnr, desc = "vim.lsp.buf.definition()", })
+
+			vim.keymap.set("n", "grf", vim.lsp.buf.format,
+			{ buffer = bufnr, desc = "vim.lsp.buf.format()", })
+		end,
+	})
+end
+
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
+	virtual_text = true,
+	signs = true,
+	underline = true,
 })
 
 
 vim.lsp.enable({
 	"lua_ls",
 	"ts_ls",
-	"zls",
+	-- "zls",
 })
